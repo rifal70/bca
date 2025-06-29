35ef5cde89c825dc4bb8bde623a1294c8d93e358
rifal70 <rifqinaufal70@gmail.com>
2025-06-29 22:49:29 +0700

    add validation rules on InputService, add validationDownPayment on LoanCalculator n adjust file_inputs.txt

--- src/main/java/org/example/controller/LoanCalculator.java
+++ src/main/java/org/example/controller/LoanCalculator.java
@@ -5,6 +5,7 @@ import org.example.model.LoanInput;
 
 public class LoanCalculator {
     public void calculate(LoanInput input) {
+        validateDownPayment(input);
         double baseRate = InterestRateFactory.getBaseRate(input.getVehicleType());
 
         System.out.println("Hasil simulasi pinjaman:");
@@ -17,6 +18,23 @@ public class LoanCalculator {
 
     private double getRateIncrement(int year) {
         return (year / 2) * 0.5 + (year % 2) * 0.1;
+    }
+
+    private void validateDownPayment(LoanInput input) {
+        if (input.getCondition().equalsIgnoreCase("baru")) {
+            double minPercent = input.getVehicleType().equalsIgnoreCase("mobil") ? 0.35 : 0.25;
+            long minDP = (long) (minPercent * input.getLoanAmount());
+
+            if (input.getDownPayment() < minDP) {
+                throw new IllegalArgumentException(String.format(
+                        "DP minimal untuk %s baru adalah %.0f%% dari jumlah pinjaman (Minimal: Rp %,d). Inputan Anda: Rp %,d",
+                        input.getVehicleType(),
+                        minPercent * 100,
+                        minDP,
+                        input.getDownPayment()
+                ));
+            }
+        }
     }
 
 }
--- src/main/java/org/example/service/InputService.java
+++ src/main/java/org/example/service/InputService.java
@@ -4,6 +4,7 @@ import org.example.model.LoanInput;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Year;
 import java.util.Scanner;
 
 public class InputService {
@@ -13,17 +14,41 @@ public class InputService {
         System.out.print("Jenis Kendaraan (Mobil/Motor): ");
         String vehicleType = sc.nextLine();
 
+        if (!vehicleType.equalsIgnoreCase("mobil") && !vehicleType.equalsIgnoreCase("motor")) {
+            throw new IllegalArgumentException("Pilihan yang tersedia hanya mobil atau motor.");
+        }
+
         System.out.print("Kondisi (Baru/Bekas): ");
         String condition = sc.nextLine();
+
+        if (!condition.equalsIgnoreCase("baru") && !condition.equalsIgnoreCase("bekas")) {
+            throw new IllegalArgumentException("Pilihan yang tersedia hanya baru atau bekas.");
+        }
 
         System.out.print("Tahun Kendaraan: ");
         int year = Integer.parseInt(sc.nextLine());
 
+        if (year < 1000 || year > 9999) {
+            throw new IllegalArgumentException("Tahun Kendaraan wajib 4 digit angka.");
+        }
+
+        if (condition.equalsIgnoreCase("baru") && year < Year.now().getValue() - 1) {
+            throw new IllegalArgumentException("Kendaraan baru wajib tahun ini atau tahun lalu.");
+        }
+
         System.out.print("Jumlah Pinjaman: ");
         long loanAmount = Long.parseLong(sc.nextLine());
 
+        if (loanAmount > 1_000_000_000L) {
+            throw new IllegalArgumentException("Jumlah Pinjaman melebihi batas ketentuan.");
+        }
+
         System.out.print("Tenor (1-6 tahun): ");
         int tenor = Integer.parseInt(sc.nextLine());
+
+        if (tenor < 1 || tenor > 6) {
+            throw new IllegalArgumentException("Tenor tidak bisa kurang dari 1 tahun dan lebih dari 6 tahun.");
+        }
 
         System.out.print("Jumlah DP: ");
         long downPayment = Long.parseLong(sc.nextLine());
@@ -39,6 +64,30 @@ public class InputService {
             long loanAmount = Long.parseLong(sc.nextLine());
             int tenor = Integer.parseInt(sc.nextLine());
             long downPayment = Long.parseLong(sc.nextLine());
+
+            if (!vehicleType.equalsIgnoreCase("mobil") && !vehicleType.equalsIgnoreCase("motor")) {
+                throw new IllegalArgumentException("Pilihan yang tersedia hanya mobil atau motor.");
+            }
+
+            if (!condition.equalsIgnoreCase("baru") && !condition.equalsIgnoreCase("bekas")) {
+                throw new IllegalArgumentException("Pilihan yang tersedia hanya baru atau bekas.");
+            }
+
+            if (year < 1000 || year > 9999) {
+                throw new IllegalArgumentException("Tahun Kendaraan wajib 4 digit angka.");
+            }
+
+            if (condition.equalsIgnoreCase("baru") && year < Year.now().getValue() - 1) {
+                throw new IllegalArgumentException("Kendaraan baru wajib tahun ini atau tahun lalu.");
+            }
+
+            if (loanAmount > 1_000_000_000L) {
+                throw new IllegalArgumentException("Jumlah Pinjaman melebihi batas ketentuan.");
+            }
+
+            if (tenor < 1 || tenor > 6) {
+                throw new IllegalArgumentException("Tenor tidak bisa kurang dari 1 tahun dan lebih dari 6 tahun.");
+            }
 
             return new LoanInput(vehicleType, condition, year, loanAmount, tenor, downPayment);
         } catch (FileNotFoundException e) {
